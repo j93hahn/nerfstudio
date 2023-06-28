92290016a9ff559a1f8ff86f344f60318249fac2
Joshua Ahn <jjahn@archimedes.ttic.edu>
2023-06-28 14:58:17 -0500

    get_sigmas() abstractmethod and viz helper funcs

--- nerfstudio/models/base_model.py
+++ nerfstudio/models/base_model.py
@@ -23,6 +23,7 @@ from collections import defaultdict
 from dataclasses import dataclass, field
 from typing import Any, Dict, List, Optional, Tuple, Type, Union
 
+import numpy as np
 import torch
 from torch import nn
 from torch.nn import Parameter
@@ -31,7 +32,8 @@ from nerfstudio.cameras.rays import RayBundle
 from nerfstudio.configs.base_config import InstantiateConfig
 from nerfstudio.configs.config_utils import to_immutable_dict
 from nerfstudio.data.scene_box import SceneBox
-from nerfstudio.engine.callbacks import TrainingCallback, TrainingCallbackAttributes
+from nerfstudio.engine.callbacks import (TrainingCallback,
+                                         TrainingCallbackAttributes)
 from nerfstudio.model_components.scene_colliders import NearFarCollider
 
 
@@ -128,6 +130,19 @@ class Model(nn.Module):
             Outputs of model. (ie. rendered colors)
         """
 
+    @abstractmethod
+    def get_sigmas(self, ray_bundle: RayBundle) -> Dict[str, Union[torch.Tensor, List]]:
+        """Obtain the sigmas for the ray bundle
+
+        Modeled after the get_outputs() method.
+
+        Args:
+            ray_bundle: ray bundle to compute sigmas for
+
+        Returns:
+            sigmas for the ray bundle
+        """
+
     def forward(self, ray_bundle: RayBundle) -> Dict[str, Union[torch.Tensor, List]]:
         """Run forward starting with a ray bundle. This outputs different things depending on the configuration
         of the model and whether or not the batch is provided (whether or not we are training basically)
@@ -186,6 +201,134 @@ class Model(nn.Module):
         for output_name, outputs_list in outputs_lists.items():
             outputs[output_name] = torch.cat(outputs_list).view(image_height, image_width, -1)  # type: ignore
         return outputs
+
+    @torch.no_grad()
+    def get_sigmas_from_camera_ray_bundle(self, camera_ray_bundle: RayBundle) -> Dict[str, torch.Tensor]:
+        """Takes in camera parameters and computes the sigmas at a given percentile range of the model's
+        outputted weights from the volumetric rendering equation along each ray.
+
+        Modeled after the get_outputs_for_camera_ray_bundle() function.
+
+        Args:
+            camera_ray_bundle: ray bundle to calculate outputs over
+        """
+        num_rays_per_chunk = self.config.eval_num_rays_per_chunk
+        image_height, image_width = camera_ray_bundle.origins.shape[:2]
+        num_rays = len(camera_ray_bundle)
+        outputs_lists = defaultdict(list)
+        for i in range(0, num_rays, num_rays_per_chunk):
+            start_idx = i
+            end_idx = i + num_rays_per_chunk
+            ray_bundle = camera_ray_bundle.get_row_major_sliced_ray_bundle(start_idx, end_idx)
+            if self.collider is not None:
+                ray_bundle = self.collider(ray_bundle)
+            outputs = self.get_sigmas(ray_bundle)
+            for output_name, output in outputs.items():  # type: ignore
+                outputs_lists[output_name].append(output)
+        outputs = {}
+        for output_name, outputs_list in outputs_lists.items():
+            outputs[output_name] = np.concatenate(outputs_list).reshape(image_height, image_width, -1)  # type: ignore
+        return outputs
+
+    def viz_histograms(
+        self,
+        weights: np.ndarray,
+        sigmas: np.ndarray,
+        xyz_samples: np.ndarray,
+        percentile: float = 0.5
+    ) -> Tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray]:
+        """Compute the weights histogram of multiple rays simultaneously
+
+        Args:
+            weights: N_rays x N_samples
+            sigmas: N_rays x N_samples
+            xyz_samples: N_rays x N_samples x 3
+            percentile: percentile to compute the value at
+
+        Returns:
+            indices of weights histogram
+            weights at indices
+            sigmas at indices
+            xyz locations at indices
+        """
+        _idxs = np.zeros(weights.shape[0], dtype=np.int32)
+        _weights = np.zeros(weights.shape[0], dtype=np.float32)
+        _sigmas = np.zeros(weights.shape[0], dtype=np.float32)
+        _xyz_locs = np.zeros((weights.shape[0], 3), dtype=np.float32)
+
+        # mask will store the indices of the rays that have not yet been processed
+        # as false; once a ray has been processed, its index will be set to true
+        mask = np.zeros(weights.shape[0], dtype=bool)
+        _weights_sum = weights.sum(axis=-1)
+
+        # if the sum of the weights is 0, the ray passed through empty space; apply a
+        # mask to that ray as it will not contribute to the final image
+        if np.any(_weights_sum == 0):
+            mask[_weights_sum == 0] = True
+            if mask.sum() == weights.shape[0]:
+                return _idxs, _weights, _sigmas, _xyz_locs
+
+        # return the maximum weight if it is >= 50% of the total sum of the weights; this
+        # value must divide the weights histogram into two equal parts
+        np.seterr(divide='ignore', invalid='ignore')    # ignore divide by zero warnings
+        if np.any((weights[~mask].max(axis=-1) / _weights_sum[~mask]) >= 0.5):
+            _wmax = np.nan_to_num(weights.max(axis=-1) / _weights_sum, nan=0.0)
+            _wmax = (_wmax >= 0.5) & (~mask)
+            _idxs[_wmax] = weights.argmax(axis=-1)[_wmax]
+            _weights[_wmax] = weights[_wmax, _idxs[_wmax]]
+            _sigmas[_wmax] = sigmas[_wmax, _idxs[_wmax]]
+            _xyz_locs[_wmax] = xyz_samples[_wmax, _idxs[_wmax]]
+
+            # apply a mask to the rays that have been processed
+            mask[_wmax] = True
+            if mask.sum() == weights.shape[0]:
+                return _idxs, _weights, _sigmas, _xyz_locs
+
+        # normalize the weights of each ray to sum 1 and compute its cumulative distribution function
+        weights_cum = np.nan_to_num(weights / _weights_sum[..., None], nan=0.0)
+        weights_cum = np.cumsum(weights_cum, axis=-1)
+
+        # extract the first weight value at the specified percentile of the CDF for each ray
+        weights_cum = (weights_cum >= percentile)
+        _wpercentile = np.argmax(weights_cum, axis=-1) # argmax returns the first index where the condition is met
+
+        # store the index, weight, and xyz location of the given ray
+        _idxs[~mask] = _wpercentile[~mask]
+        _weights[~mask] = weights[~mask, _idxs[~mask]]
+        _sigmas[~mask] = sigmas[~mask, _idxs[~mask]]
+        _xyz_locs[~mask] = xyz_samples[~mask, _idxs[~mask]]
+
+        # turn divide by zero warnings back on
+        np.seterr(divide='warn', invalid='warn')
+
+        # return the indices, weights, and xyz locations of the rays
+        return _idxs, _weights, _sigmas, _xyz_locs
+
+    def create_single_sigma_viz(self, _sigmas, pose, height, width):
+        import matplotlib.pyplot as plt
+        from matplotlib.colors import AsinhNorm, LogNorm, Normalize
+        from mpl_toolkits.axes_grid1 import ImageGrid
+
+        fig = plt.figure(figsize=(5, 6))
+        grid = ImageGrid(
+            fig, 111, nrows_ncols=(1, 1),
+            cbar_location="right", cbar_mode="edge", cbar_size="7%", cbar_pad=0.15,
+        )
+
+        lower = 1e-2
+        upper = 1e4
+
+        _sigmas[_sigmas < lower] = lower
+        _sigmas[_sigmas > upper] = upper
+
+        h = grid[0].imshow(_sigmas.reshape(height, width), cmap='viridis', norm=LogNorm(lower, upper))
+        grid[0].set_title(f'Sigma Visualizations at Pose {pose}')
+        grid[0].get_xaxis().set_visible(False)
+        grid[0].get_yaxis().set_visible(False)
+
+        plt.colorbar(h, cax=grid.cbar_axes[0])
+        plt.savefig(f'sigmas_pose{pose}.png', dpi=300)
+        plt.close()
 
     @abstractmethod
     def get_image_metrics_and_images(
