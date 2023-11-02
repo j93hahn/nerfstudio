6b3330db587cb0a9063f558f74c22d12d7060135
Joshua Ahn <jjahn@archimedes.ttic.edu>
2023-11-02 13:05:37 -0500

    update file

--- nerfstudio/scripts/sigmas.py
+++ nerfstudio/scripts/sigmas.py
@@ -1,18 +1,3 @@
-# Copyright 2022 the Regents of the University of California, Nerfstudio Team and contributors. All rights reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-#!/usr/bin/env python
 """
 sigmas.py - modified from eval.py
 """
@@ -46,6 +31,12 @@ class RenderSigmas:
 
     def main(self) -> None:
         """Main function."""
+        # import torch
+        # from scratch.algos.grid import sample_grid_box
+        # _, pipeline, _, _ = eval_setup(self.load_config)
+        # pts = sample_grid_box(torch.tensor([200,200,200]), torch.device('cuda'))
+        # sigma = pipeline.model.field.get_density_at_positions(pts.reshape(-1, 1, 3))
+        # breakpoint()
 
         assert self.save_xyz or self.load_xyz, "Must either save or load xyz locations."
         mode: str
